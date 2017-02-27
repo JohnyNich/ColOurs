b7ac116bf94c1ec55588eb1d140162adf51451db
Jonathan Nicholls <jonathanrnicholls@gmail.com>
2017-02-27 18:32:50 +0800

    Added all colour classes
    I’ve added all the colour classes, including one called Normal, which
    is if you want to set the colour back to normal or if you want to
    create, for instance, underline text in the normal colour.

--- colours.py
+++ colours.py
@@ -1,10 +1,21 @@
-black = "\033[0;30;49m"
-green = "\033[0;32;49m"
-yellow = "\033[0;33;49m"
-blue = "\033[0;34;49m"
-purple = "\033[0;35;49m"
-cyan = "\033[0;36;49m"
-white = "\033[0;37;49m"
+class Normal:
+	normal = "\033[0;39;49m"
+	bold = "\033[1;39;49m"
+	faint = "\033[2;39;49m"
+	italic = "\033[3;39;49m"
+	underline = "\033[4;39;49m"
+	blinking = "\033[5;39;49m"
+	reverse = "\033[7;39;49m"
+	invisible = "\033[8;39;49m"
+class Black:
+	normal = "\033[0;30;49m"
+	bold = "\033[1;30;49m"
+	faint = "\033[2;30;49m"
+	italic = "\033[3;30;49m"
+	underline = "\033[4;30;49m"
+	blinking = "\033[5;30;49m"
+	reverse = "\033[7;30;49m"
+	invisible = "\033[8;30;49m"
 class Red:
 	normal = "\033[0;31;49m"
 	bold = "\033[1;31;49m"
@@ -14,12 +25,66 @@ class Red:
 	blinking = "\033[5;31;49m"
 	reverse = "\033[7;31;49m"
 	invisible = "\033[8;31;49m"
-print ("I'm normal")
-print (black + "I'm black!")
-print (Red.normal + "I'm red!")
-print (green + "I'm green!")
-print (yellow + "I'm yellow!")
-print (blue + "I'm blue!")
-print (purple + "I'm purple!")
-print (cyan + "I'm cyan!")
-print (white + "I'm white!")
+class Green:
+	normal = "\033[0;32;49m"
+	bold = "\033[1;32;49m"
+	faint = "\033[2;32;49m"
+	italic = "\033[3;32;49m"
+	underline = "\033[4;32;49m"
+	blinking = "\033[5;32;49m"
+	reverse = "\033[7;32;49m"
+	invisible = "\033[8;32;49m"
+class Yellow:
+	normal = "\033[0;33;49m"
+	bold = "\033[1;33;49m"
+	faint = "\033[2;33;49m"
+	italic = "\033[3;33;49m"
+	underline = "\033[4;33;49m"
+	blinking = "\033[5;33;49m"
+	reverse = "\033[7;33;49m"
+	invisible = "\033[8;33;49m"
+class Blue:
+	normal = "\033[0;34;49m"
+	bold = "\033[1;34;49m"
+	faint = "\033[2;34;49m"
+	italic = "\033[3;34;49m"
+	underline = "\033[4;34;49m"
+	blinking = "\033[5;34;49m"
+	reverse = "\033[7;34;49m"
+	invisible = "\033[8;34;49m"
+class Purple:
+	normal = "\033[0;35;49m"
+	bold = "\033[1;35;49m"
+	faint = "\033[2;35;49m"
+	italic = "\033[3;35;49m"
+	underline = "\033[4;35;49m"
+	blinking = "\033[5;35;49m"
+	reverse = "\033[7;35;49m"
+	invisible = "\033[8;35;49m"
+class Cyan:
+	normal = "\033[0;36;49m"
+	bold = "\033[1;36;49m"
+	faint = "\033[2;36;49m"
+	italic = "\033[3;36;49m"
+	underline = "\033[4;36;49m"
+	blinking = "\033[5;36;49m"
+	reverse = "\033[7;36;49m"
+	invisible = "\033[8;36;49m"
+class White:
+	normal = "\033[0;37;49m"
+	bold = "\033[1;37;49m"
+	faint = "\033[2;37;49m"
+	italic = "\033[3;37;49m"
+	underline = "\033[4;37;49m"
+	blinking = "\033[5;37;49m"
+	reverse = "\033[7;37;49m"
+	invisible = "\033[8;37;49m"
+#~ print (Normal.normal)
+#~ print (Black.normal + "I'm black!")
+#~ print (Red.normal + "I'm red!")
+#~ print (Green.normal + "I'm green!")
+#~ print (Yellow.normal + "I'm yellow!")
+#~ print (Blue.normal + "I'm blue!")
+#~ print (Purple.normal + "I'm purple!")
+#~ print (Cyan.normal + "I'm cyan!")
+#~ print (White.normal + "I'm white!")
