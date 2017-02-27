288653e56fd34e2c368b8690179955c3c3766cb5
Jonathan Nicholls <jonathanrnicholls@gmail.com>
2017-02-27 17:52:42 +0800

    Uploaded current python file to repository
    I’ve uploaded the current python fileI’ve been working on to this
    GitHub repository.

---
+++ colours.py
@@ -0,0 +1,14 @@
+red = "\033[0;31;49m"
+green = "\033[0;32;49m"
+yellow = "\033[0;33;49m"
+blue = "\033[0;34;49m"
+purple = "\033[0;35;49m"
+cyan = "\033[0;36;49m"
+print ("I'm normal")
+print (red + "I'm red!")
+print (green + "I'm green!")
+print (yellow + "I'm yellow!")
+print (blue + "I'm blue!")
+print (purple + "I'm purple!")
+print (cyan + "I'm cyan!")
+
