87c41bc26f38195d7f907d49e046cd1d53a8b0c6
Jonathan Nicholls <jonathanrnicholls@gmail.com>
2017-02-27 17:55:16 +0800

    Added the rest of the colours
    I’ve added the rest of the ANSI colours that are available to the code.

--- colours.py
+++ colours.py
@@ -1,14 +1,18 @@
+black = "\033[0;30;49m"
 red = "\033[0;31;49m"
 green = "\033[0;32;49m"
 yellow = "\033[0;33;49m"
 blue = "\033[0;34;49m"
 purple = "\033[0;35;49m"
 cyan = "\033[0;36;49m"
+white = "\033[0;37;49m"
 print ("I'm normal")
+print (black + "I'm black!")
 print (red + "I'm red!")
 print (green + "I'm green!")
 print (yellow + "I'm yellow!")
 print (blue + "I'm blue!")
 print (purple + "I'm purple!")
 print (cyan + "I'm cyan!")
+print (white + "I'm white!")
 
