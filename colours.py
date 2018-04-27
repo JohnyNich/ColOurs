2f3450cb87c6100c73f48010b53f4d8886c00350
Kevin Wylie <kvnwyl@gmail.com>
2018-04-27 20:04:42 +0100

    Encapsulated logic into classes and optimised code.

--- colours.py
+++ colours.py
@@ -1,90 +1,72 @@
-class Normal:
-	normal = "\033[0;39;49m"
-	bold = "\033[1;39;49m"
-	faint = "\033[2;39;49m"
-	italic = "\033[3;39;49m"
-	underline = "\033[4;39;49m"
-	blinking = "\033[5;39;49m"
-	reverse = "\033[7;39;49m"
-	invisible = "\033[8;39;49m"
-class Black:
-	normal = "\033[0;30;49m"
-	bold = "\033[1;30;49m"
-	faint = "\033[2;30;49m"
-	italic = "\033[3;30;49m"
-	underline = "\033[4;30;49m"
-	blinking = "\033[5;30;49m"
-	reverse = "\033[7;30;49m"
-	invisible = "\033[8;30;49m"
-class Red:
-	normal = "\033[0;31;49m"
-	bold = "\033[1;31;49m"
-	faint = "\033[2;31;49m"
-	italic = "\033[3;31;49m"
-	underline = "\033[4;31;49m"
-	blinking = "\033[5;31;49m"
-	reverse = "\033[7;31;49m"
-	invisible = "\033[8;31;49m"
-class Green:
-	normal = "\033[0;32;49m"
-	bold = "\033[1;32;49m"
-	faint = "\033[2;32;49m"
-	italic = "\033[3;32;49m"
-	underline = "\033[4;32;49m"
-	blinking = "\033[5;32;49m"
-	reverse = "\033[7;32;49m"
-	invisible = "\033[8;32;49m"
-class Yellow:
-	normal = "\033[0;33;49m"
-	bold = "\033[1;33;49m"
-	faint = "\033[2;33;49m"
-	italic = "\033[3;33;49m"
-	underline = "\033[4;33;49m"
-	blinking = "\033[5;33;49m"
-	reverse = "\033[7;33;49m"
-	invisible = "\033[8;33;49m"
-class Blue:
-	normal = "\033[0;34;49m"
-	bold = "\033[1;34;49m"
-	faint = "\033[2;34;49m"
-	italic = "\033[3;34;49m"
-	underline = "\033[4;34;49m"
-	blinking = "\033[5;34;49m"
-	reverse = "\033[7;34;49m"
-	invisible = "\033[8;34;49m"
-class Purple:
-	normal = "\033[0;35;49m"
-	bold = "\033[1;35;49m"
-	faint = "\033[2;35;49m"
-	italic = "\033[3;35;49m"
-	underline = "\033[4;35;49m"
-	blinking = "\033[5;35;49m"
-	reverse = "\033[7;35;49m"
-	invisible = "\033[8;35;49m"
-class Cyan:
-	normal = "\033[0;36;49m"
-	bold = "\033[1;36;49m"
-	faint = "\033[2;36;49m"
-	italic = "\033[3;36;49m"
-	underline = "\033[4;36;49m"
-	blinking = "\033[5;36;49m"
-	reverse = "\033[7;36;49m"
-	invisible = "\033[8;36;49m"
-class White:
-	normal = "\033[0;37;49m"
-	bold = "\033[1;37;49m"
-	faint = "\033[2;37;49m"
-	italic = "\033[3;37;49m"
-	underline = "\033[4;37;49m"
-	blinking = "\033[5;37;49m"
-	reverse = "\033[7;37;49m"
-	invisible = "\033[8;37;49m"
-#~ print (Normal.normal)
-#~ print (Black.normal + "I'm black!")
-#~ print (Red.normal + "I'm red!")
-#~ print (Green.normal + "I'm green!")
-#~ print (Yellow.normal + "I'm yellow!")
-#~ print (Blue.normal + "I'm blue!")
-#~ print (Purple.normal + "I'm purple!")
-#~ print (Cyan.normal + "I'm cyan!")
-#~ print (White.normal + "I'm white!")
+import sys 
+
+class colour_print:
+    colours_dict = {
+        "normal": 39,
+        "black": 30,
+        "white": 37,
+        "red": 31,
+        "green": 32,
+        "blue": 34,
+        "yellow": 33,
+        "cyan": 36,
+        "purple": 35
+    }
+
+    styles_dict = {
+        "normal": 0,
+        "bold": 1,
+        "faint": 2,
+        "italic": 3,
+        "underline": 4,
+        "blinking": 5,
+        "reverse": 7,
+        "invisible": 8
+    }
+
+class Meta(type):
+    def __getattr__(cls, attr):
+        try:
+            print_colour = colour_print.colours_dict.get(cls.__name__.lower())
+            print_style = colour_print.styles_dict.get(attr.lower())
+            return "\033[{0};{1};49m".format(print_style, print_colour)
+
+        except (AttributeError, KeyError):
+            return None
+
+class Normal(metaclass=Meta):
+    pass
+
+class Black(metaclass=Meta):
+    pass
+
+class Red(metaclass=Meta):
+    pass
+
+class Green(metaclass=Meta):
+    pass
+
+class Yellow(metaclass=Meta):
+    pass
+
+class Blue(metaclass=Meta):
+    pass
+
+class Purple(metaclass=Meta):
+    pass
+
+class Cyan(metaclass=Meta):
+    pass
+
+class White(metaclass=Meta):
+    pass
+
+print (Normal.normal)
+print (Black.normal + "I'm black!")
+print (Red.normal + "I'm red!")
+print (Green.normal + "I'm green!")
+print (Yellow.normal + "I'm yellow!")
+print (Blue.normal + "I'm blue!")
+print (Purple.normal + "I'm purple!")
+print (Cyan.normal + "I'm cyan!")
+print (White.normal + "I'm white!")
